599f28f9852aaa815a068cc187d7b24b93d7a9fd
Gisellesanchez <162376632+Gisellesanchez@users.noreply.github.com>
2025-04-25 11:35:52 -0600

    Update Program.cs

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -5,5 +5,11 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Sandbox World!");
+
+        Console.WriteLine("Please input your name:  ");
+        string firstName = Console.ReadLine();
+        Console.WriteLine($"Your name is: {firstName}  ");
+        string lastName = Console.ReadLine();
+        Console.WriteLine($"Your name is: {lastName}, {firstName}  ");g
     }
 }
